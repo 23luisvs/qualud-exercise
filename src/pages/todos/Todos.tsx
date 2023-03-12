c2684057b2959534355e42c2fc4285a50a8f9454
Luis Alberto Vázquez Serrano <23luisvs@gmail.com>
2023-03-12 06:53:28 -0400

    change-status-delete-todo

--- src/pages/todos/Todos.tsx
+++ src/pages/todos/Todos.tsx
@@ -1,7 +1,8 @@
+import { Haptics } from "@capacitor/haptics";
 import {
+  IonButton,
   IonCard,
   IonCardContent,
-  IonCardHeader,
   IonCardSubtitle,
   IonCardTitle,
   IonCol,
@@ -11,12 +12,14 @@ import {
   IonIcon,
   IonItem,
   IonLabel,
+  IonLoading,
   IonPage,
   IonRow,
-  IonTabBar,
   IonToggle,
+  IonToolbar,
+  useIonActionSheet,
 } from "@ionic/react";
-import { checkmarkCircle, ellipse } from "ionicons/icons";
+import { checkmarkCircle, ellipse, trash } from "ionicons/icons";
 import { useEffect, useState } from "react";
 import Header from "../../components/Header";
 import CreateTodo from "../../components/todos/CreateTodo";
@@ -27,9 +30,59 @@ import { useAuth } from "../../store/AuthContext";
 const Todos: React.FC = () => {
   const { user } = useAuth();
   const [todos, setTodos] = useState<Todo[]>([]);
+  const [loading, setLoading] = useState(false);
+  const [present] = useIonActionSheet();
   useEffect(() => {
     if (user) setTodos(user.todos.nodes);
   }, [user]);
+  //action para cambiar de estado
+  const changeTodoStatus = (index: number) => {
+    setLoading(true);
+    setTimeout(() => {
+      setLoading(false);
+      let cloneTodos: Todo[] = [...todos];
+      cloneTodos[index] = {
+        id: todos[index].id,
+        status: todos[index].status === "completed" ? "pending" : "completed",
+        dueOn: todos[index].dueOn,
+        title: todos[index].title,
+      };
+      setTodos(cloneTodos);
+    }, 1000);
+  };
+  const deleteHandler = async (index: number) => {
+    await Haptics.vibrate();
+    present({
+      header: "Confirm the action",
+      buttons: [
+        {
+          text: "Delete",
+          role: "destructive",
+          data: {
+            action: "delete",
+          },
+        },
+        {
+          text: "Cancel",
+          role: "cancel",
+          data: {
+            action: "cancel",
+          },
+        },
+      ],
+      onDidDismiss: async ({ detail }) => {
+        if (detail.role === "destructive") {
+          setLoading(true);
+          setTimeout(() => {
+            setLoading(false);
+            let cloneTodos: Todo[] = [...todos];
+            cloneTodos.splice(index, 1);
+            setTodos(cloneTodos);
+          }, 1000);
+        }
+      },
+    });
+  };
   return (
     <IonPage>
       <Header pageTitle="Todos" backButton={true} />
@@ -38,18 +91,13 @@ const Todos: React.FC = () => {
           {todos.map((todo: Todo, index: number) => {
             return (
               <IonCard key={index}>
-                <IonCardHeader>
-                  <IonCardTitle>{todo.title}</IonCardTitle>
-                  <IonCardSubtitle className="ion-text-end">
-                    {"Date: " + showDate(new Date(todo.dueOn))}
-                  </IonCardSubtitle>
-                </IonCardHeader>
                 <IonCardContent>
                   <IonGrid>
                     <IonRow>
                       <IonCol>
                         <IonItem>
                           <IonIcon
+                            className="ion-margin-bottom"
                             slot="start"
                             icon={
                               todo.status === "completed"
@@ -63,10 +111,31 @@ const Todos: React.FC = () => {
                           <IonLabel>Is {todo.status}</IonLabel>
                           <IonToggle
                             checked={todo.status === "completed" ? true : false}
-                            onIonChange={() => console.log("change")}
+                            onIonChange={() => changeTodoStatus(index)}
                             slot="end"
                           ></IonToggle>
                         </IonItem>
+                      </IonCol>
+                    </IonRow>
+                    <IonRow>
+                      <IonCol>
+                        <IonCardTitle>{todo.title}</IonCardTitle>
+                        <IonCardSubtitle className="ion-text-end">
+                          {"Date: " + showDate(new Date(todo.dueOn))}
+                        </IonCardSubtitle>
+                      </IonCol>
+                    </IonRow>
+                    <IonRow>
+                      <IonCol>
+                        <IonButton
+                          className="ion-padding-end"
+                          size="small"
+                          color="danger"
+                          onClick={() => deleteHandler(index)}
+                        >
+                          <IonIcon slot="start" icon={trash} />
+                          <IonLabel>Delete</IonLabel>
+                        </IonButton>
                       </IonCol>
                     </IonRow>
                   </IonGrid>
@@ -74,14 +143,21 @@ const Todos: React.FC = () => {
               </IonCard>
             );
           })}
-          <CreateTodo todos={todos} setTodos={setTodos} />
         </div>
+        <CreateTodo todos={todos} setTodos={setTodos} />
       </IonContent>
       <IonFooter>
-        <IonTabBar>
-          <IonLabel>{showTodosQuantity(todos.length)}</IonLabel>
-        </IonTabBar>
+        <IonToolbar>
+          <IonItem>
+            <IonLabel>{showTodosQuantity(todos.length)}</IonLabel>
+          </IonItem>
+        </IonToolbar>
       </IonFooter>
+      <IonLoading
+        cssClass="my-custom-class"
+        isOpen={loading}
+        message={"Please wait..."}
+      />
     </IonPage>
   );
 };
